f0d5cd2f107a2704f6c22306ee37b431c47b1cfc
elamboatonge <elamboatonge@gmail.com>
2021-07-09 14:44:13 +0300

    updates

--- src/components/ConnectConference.vue
+++ src/components/ConnectConference.vue
@@ -2,13 +2,13 @@
   <div>
     <v-container class="py-10">
       <v-row class="justify-center">
-        <v-col cols="12" lg="6">
+        <v-col cols="12" lg="7">
           <div class="text-h5 setting-column">
             <v-row class="justify-center">
               <v-col cols="12" class="d-flex">
                 <v-icon large @click="RouteConference">mdi-arrow-left</v-icon>
-                <div class="text-h5 mx-auto">
-                  <span class="ml-n10"> Подключение к конференции </span>
+                <div class="text-h4 mx-auto">
+                  <span class=""> Подключение к конференции </span>
                 </div>
               </v-col>
             </v-row>
