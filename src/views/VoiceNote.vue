aceec254e1a60f32f4227998ed55ab0d29f9c1b2
elamboatonge <elamboatonge@gmail.com>
2021-09-10 05:15:26 +0300

    improves voice_note

--- src/views/VoiceNote.vue
+++ src/views/VoiceNote.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container class="py-10 mhide">
       <v-row>
-        <v-col cols="12">
+        <v-col cols="12" class="d-flex justify-space-between">
           <v-icon
             dark
             class="orange pa-3 rounded-xl"
@@ -11,11 +11,15 @@
             {{ recording ? "mdi-stop" : "mdi-microphone-outline" }}
           </v-icon>
           <audio ref="audio"></audio>
+          <v-spacer></v-spacer>
+          <v-btn class="pa-6" text icon color="orange">
+            <v-icon large @click="createDialog()">mdi-note-plus-outline</v-icon>
+          </v-btn>
         </v-col>
       </v-row>
 
       <v-row class="voice-border">
-        <v-col md="4" lg="2">
+        <v-col md="4" lg="3">
           <v-navigation-drawer permanent height="500px" color="transparent">
             <template v-slot:prepend>
               <v-list-item>
@@ -27,20 +31,66 @@
 
             <v-divider></v-divider>
 
-            <v-list dense>
-              <v-list-item v-for="item in items" :key="item.title" link>
-                <v-list-item-icon>
-                  <v-icon>mdi-folder</v-icon>
-                </v-list-item-icon>
-
-                <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+            <v-list dense light>
+              <div class="py-2" v-for="item in archive_items" :key="item.title">
+                <!--                <v-list-item-icon>-->
+                <!--                  <v-icon>mdi-folder</v-icon>-->
+                <!--                </v-list-item-icon>-->
+                <div class="d-flex justify-space-around">
+                  <div>
+                    <v-btn text>
+                      {{ item.title }}
+                    </v-btn>
+                  </div>
+                  <div class="my-auto">
+                    <v-icon @click.prevent="openEditDialog(item.id)">
+                      mdi-square-edit-outline
+                    </v-icon>
+                    <v-icon
+                      class="ml-1"
+                      @click.stop="openDeleteDialog(item.id)"
+                    >
+                      mdi-delete-forever-outline
+                    </v-icon>
+                  </div>
+                </div>
+              </div>
             </v-list>
           </v-navigation-drawer>
         </v-col>
-        <v-col md="8" lg="10">
+        <v-col md="8" lg="9">
+          <v-dialog v-model="deleteDialog" persistent max-width="600px">
+            <v-card>
+              <v-card-title class="justify-center">
+                <span class="text-h5">Delete</span>
+              </v-card-title>
+              <v-card-text class="pb-0">
+                <v-container> Are you sure you want to delete</v-container>
+              </v-card-text>
+              <v-card-actions class="px-9 pb-7">
+                <v-btn
+                  elevation="0"
+                  dark
+                  color="blue darken-1"
+                  class="px-5"
+                  rounded
+                  @click="deleteChoice = true"
+                >
+                  Yes
+                </v-btn>
+
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  rounded
+                  text
+                  @click="deleteDialog = false"
+                >
+                  No
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
           <tiny-editor :editorText="editorText"></tiny-editor>
         </v-col>
       </v-row>
@@ -86,7 +136,7 @@
             <v-divider></v-divider>
 
             <v-list dense>
-              <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item v-for="item in archive_items" :key="item.title" link>
                 <v-list-item-icon>
                   <v-icon>mdi-folder</v-icon>
                 </v-list-item-icon>
@@ -128,17 +178,42 @@ export default {
     return {
       recording: false,
       speechId: "",
-      items: [
-        { title: "#803762003" },
-        { title: "#797518030" },
-        { title: "#752856189" },
+      archive_items: [
+        {
+          title: "#803762003",
+          id: "1",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+          title: "#797518030",
+          id: "2",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+          title: "#752856189",
+          id: "3",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
       ],
       editorText: "",
       blobs: [],
       transcriptList: [],
+      deleteDialog: false,
+      editDialog: false,
+      deleteChoice: 0,
     };
   },
   methods: {
+    openEditDialog() {},
+    openDeleteDialog(id) {
+      this.deleteDialog = true;
+      if (this.deleteChoice) {
+        this.archive_items = this.archive_items.filter(
+          (item) => item.id !== id
+        );
+        //  TODO: Delete from the backend
+      }
+    },
     renderResponse(response) {
       response.results.forEach((r) => {
         const len = this.transcriptList.length;
@@ -167,9 +242,9 @@ export default {
       this.editorText = "";
       this.transcriptList.forEach((r) => {
         if (r.isFinal) {
-          this.editorText += " < " + r.transcript + " > <br>";
+          this.editorText += r.transcript + "<br>";
         } else {
-          this.editorText += " < " + r.transcript + " >";
+          this.editorText += r.transcript;
         }
       });
     },
